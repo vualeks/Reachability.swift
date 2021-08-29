6df2c64192c38574917b64dfea4ca8846c5db910
Aleksandar Vukovic <vualeks@gmail.com>
2021-08-29 02:11:49 +0200

    Updated SPM Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Reachability",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
     products: [
         .library(
             name: "Reachability",
